c55ecc5695348c5824f9b5bb0a9962806e978aae
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-10-02 19:00:30 +0900

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -11,8 +11,6 @@
 
 # variables #
 log=/home/$USER/log
-dconf=/etc/dnf/dnf.conf
-
 
 # functions #
 
@@ -30,16 +28,10 @@ fi
 function dnf-setup(){
 
     {
-    gpgcheck=2
-    installonly_limit=4
-    clean_requirements_on_remove=True
-    best=True
-    skip_if_unavailable=False
-
     # custom edits
     max_parallel_downloads=21
     fastestmirror=True
-    } > $dconf
+    } >> /etc/dnf/dnf.conf
 
 }
 
@@ -47,7 +39,7 @@ function dnf-setup(){
 function update(){
 
     dnf upgrade -y && dnf install epel-release -y\
-    && dnf install dnf install ranger ncdu\
+    && dnf install ranger ncdu\
     vim dnf-automatic policycoreutils-python-utils bash-completion -y
 
 }
