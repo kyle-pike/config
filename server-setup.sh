b6c5e29dca19af8064aac1df8feb4aa1145cc673
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-10-02 18:39:28 +0900

    Update server-setup.sh

--- server-setup.sh
+++ server-setup.sh
@@ -3,6 +3,11 @@
 ### Pike's Server config ###
 ### requirements, rhel based server ### 
 
+# goals
+# move to ansible 
+# put updates on log and to standard output 
+#
+
 
 # variables #
 log=/home/$USER/log
@@ -10,6 +15,16 @@ dconf=/etc/dnf/dnf.conf
 
 
 # functions #
+
+# check if root 
+function check-root(){
+
+    if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
+}
 
 # configures dnf to install faster
 function dnf-setup(){
@@ -31,8 +46,21 @@ function dnf-setup(){
 # updates system and install neccessary software 
 function update(){
 
-    dnf update && dnf install epel-release -y\
+    dnf upgrade && dnf install epel-release -y\
     && dnf install dnf install ranger ncdu\
     vim dnf-automatic policycoreutils-python-utils bash-completion -y
 
 }
+
+
+### script ### 
+
+if check-root
+
+then 
+    dnf-setup && update 
+
+else 
+    echo "script failed"
+
+fi 
