20592fd146da3c7a1455d513d23954043433b52c
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-12-18 01:34:36 +0900

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -93,13 +93,9 @@ function dock(){
 	    mkdir -p /etc/apt/keyrings
 	    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
        
-	   	echo \
-       	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-       	$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	   	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
    	   
 	  	apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-        docker run hello-world > /dev/null | grep -ie "Hello from Docker"
 
 	then echo "Docker has been installed"
 
