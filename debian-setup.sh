c739b9a7d41483825e76657b2576d872d5afcad4
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-12-18 18:39:52 +0900

    Update debian-setup.sh

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# setup's personal servers for debian based systems
-# requirement is debian installed with doas setup
+# setup personal debian servers 
 
 # variables
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
@@ -14,114 +13,99 @@ cat <<"EOF"
 
 ============================================
 
-This script will update/reboot,
-performance tune and add a mx downtime
-@0200L every Monday for this server
+ This script will update, performance tune,
+     and add a mx downtime @0200L-0230L 
+        every Monday for this server
 
 ============================================
-EOF
-sleep 10
-echo "executing script in 5 ;)"
-sleep 1
-echo 4 
-sleep 1
-echo 3
-sleep 1
-echo 2 
-sleep 1
-echo 1 
 
-# ensures correct permissions to execute script and extends $PATH
+EOF
+sleep 6
+
+
+# ensures correct permissions to execute script 
 function check-root(){
 
     if [ "$EUID" -ne 0 ]
 
     then echo "incorrect permissions, Please run as root by then run the script again"; exit
 
-	fi
+    fi
 
 }
 
 
-# updates system and reboots 
+# installs system packages and updates
 function pkgs(){
 
-	 apt install curl tuned needrestart -y && apt update -y && apt upgrade -y && apt autoremove -y	
+    apt install curl tuned needrestart -y && apt update -y && apt upgrade -y && apt autoremove -y	
 
 }
 
 
 # adds to system schedule to update and reboot every monday @0200L
 function cron(){
-	
-	echo "# updates entire system and reboots every monday @0200L" >> $tab
-	echo "  0  2  *  *  1 root       apt update -y && apt upgrade -y && systemctl reboot" >> $tab
+
+    echo "# updates entire system and reboots every monday @0200L" >> $tab
+    echo "  0  2  *  *  1 root       apt update -y && apt upgrade -y && systemctl reboot" >> $tab
 
 }
 
 
-# locks root account and adds to $PATH
+# locks root account and extends $PATH
 function pass(){
 
-	echo "extending PATH, please enter your username"
-	sleep 2
-	read -p "username : " user 
-	echo "export PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin/" >> /home/"$user"/.bashrc
-	sleep 2
-	
-	echo "locking root account"
-	sleep 2	
-	passwd -l root 
-	sleep 2
+    echo "extending PATH, please enter your username"
+    sleep 2
+    read -p "username : " user 
+    echo "export PATH=$PATH:/sbin:/usr/sbin:/usr/local/sbin/:usr/local/bin/" >> /home/"$user"/.bashrc
 
-} 
+    echo "locking root account"
+    sleep 2
+    passwd -l root
+
+}
 
 
 function tail(){
 
     echo "installing tailscale vpn"
     sleep 2
-	curl -fsSL https://tailscale.com/install.sh | sh
+    curl -fsSL https://tailscale.com/install.sh | sh
 
 }
 
 
 function dock(){
 
-	if 
-		apt install ca-certificates gnupg lsb-release
-	    mkdir -p /etc/apt/keyrings
-	    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-       
-   	    echo \
- 	 	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
- 	 	$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null 
-	 
-	  	apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    echo "Installing docker"
 
-	then echo "Docker has been installed"
-
-	else echo "Docker has failed to install"
-
-	fi 
+    apt install -y ca-certificates curl gnupg lsb-release
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list >/dev/null
+    apt update -y
+    apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 }
 
 
-
 # script 
+#### include supress output, firewall
 if check-root && pass && cron && pkgs && tail && dock
 
-then
-		
-			echo "================================"
-			echo " "
-			echo "        SCRIPT COMPLETED        "		
-			echo "        REBOOTING SERVER        "				
-		    echo " "
-			echo "================================"
-			sleep 5 
-			systemctl reboot
+then cat <<"EOF"
+
+    ================================
+
+            SCRIPT COMPLETED
+            REBOOTING SERVER
+
+    ================================
+
+EOF
+    sleep 2
+    systemctl reboot
 
 else echo "script failed ;(" 
 
