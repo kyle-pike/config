44d6407ebd54588a7482e694dd13422b10f121d5
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-12-26 22:40:28 +0900

    commit

--- docker.sh
+++ docker.sh
@@ -3,7 +3,8 @@
 
 
 # variables
-CALIBRE=$(curl https://raw.githubusercontent.com/kyle-pike/config/public/apps/calibre.yml)
+CALIBRE=
+
 
 # exit if errors during script 
 set -e 
@@ -36,11 +37,11 @@ read -p "Please enter desired location for the media directory: " MEDIA
 # asks if you would like to install the desired container
 function prompt(){
 
-    read -p "Would you like to install $CONTAINER (y/n)? " answer
+    read -p "Would you like to install Calibre (y/n)? " answer
     case ${answer:0:1} in
       y|Y )
           echo "========================"
-          echo " installing $CONTAINER"
+          echo "   installing Calibre   "
           echo "========================"
           container_install
           sleep 3
@@ -60,8 +61,8 @@ function prompt(){
 # installs the container
 function container_install(){
 
-    docker network create $CONTAINER
-    docker-compose pull 
+    cd $CONFIG/$CONTAINER
+    docker-compose pull
     docker-compose up -d 
 
 }
@@ -70,6 +71,6 @@ function container_install(){
 # script
 check-root && directories
 
-for CONTAINER in $CALIBRE
+for i in 
 do prompt
 done
