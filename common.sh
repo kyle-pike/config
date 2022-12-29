9326c0a0354f3089aeee0ce07e9f53b3ee13d180
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-12-29 22:01:52 +0900

    source file for common functions/variables

---
+++ common.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+# ensures correct permissions to execute script 
+function check-root(){
+
+    if [ "$EUID" -ne 0 ]
+
+    then echo "incorrect permissions, Please run as root by then run the script again"; exit
+
+    fi
+
+}
