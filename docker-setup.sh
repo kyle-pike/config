0888f12270da2a5645468a772827d3f7918ad38c
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-12-31 01:58:56 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -47,6 +47,7 @@ done
 # adds to system schedule to update and reboot every monday @0200L
 function cron(){
 
+    chown root:root docker-update.sh; chmod 700 docker-update.sh
     mv docker-update.sh /usr/local/bin/
     echo "# updates docker containers every monday @0300L" >> $tab
     echo "  0  3  *  *  1 root       /usr/local/bin/docker-update.sh" >> $tab
