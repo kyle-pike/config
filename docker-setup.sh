110d6b3cc2e4e1ea7b4a2447c96967b12581257d
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-12-30 22:06:20 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -12,7 +12,7 @@ set -e
 # ensures correct permissions to execute script 
 function check-root(){
 
-    if [ "$EUID" -ne 0 ]
+    if [[ $UID != 0 ]]
 
     then echo "incorrect permissions, Please run as root by then run the script again"; exit
 
