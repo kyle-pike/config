4e3db9d43b61a7a5307c5eb0825a2787c27843f6
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2022-12-30 21:32:50 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -9,8 +9,16 @@ ENV_FILE=$HOME/config/apps/.env
 set -e 
 
 
-# source common functions and variables 
-source $HOME/config/common.sh
+# ensures correct permissions to execute script 
+function check-root(){
+
+    if [ "$EUID" -ne 0 ]
+
+    then echo "incorrect permissions, Please run as root by then run the script again"; exit
+
+    fi
+
+}
 
 
 # requests what directories to use for docker and places in environment file
