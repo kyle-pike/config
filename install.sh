e42a03095128e06bd71e3995de5f4f9b65b4317b
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2023-01-01 18:19:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 wget -q https://github.com/kyle-pike/config/archive/refs/tags/v1.0.tar.gz
 tar -xzf v1.0.tar.gz; rm v1.0.tar.gz
 
-mv config-1.0 config
+mv config* config
 chmod 700 config/*.sh*
 sudo config/debian-setup.sh
