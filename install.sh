213eefd9cfc297c68953acea416df52d1c0096f1
kyle-pike <114853918+kyle-pike@users.noreply.github.com>
2023-01-02 00:48:40 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # downloads config repo and runs setup script
 
-wget -q https://github.com/kyle-pike/config/archive/refs/tags/v1.0.tar.gz
-tar -xzf v1.0.tar.gz; rm v1.0.tar.gz
+wget -q https://github.com/kyle-pike/config/archive/refs/tags/v1.1.tar.gz
+tar -xzf v1.1.tar.gz; rm v1.1.tar.gz
 
 mv config* config
 chmod 700 config/*.sh*
